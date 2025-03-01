45c9f4e5fc0941b5950282ae565f7641bd6a15d3
Suhail1608 <suhailbasha1608@gmail.com>
2025-03-01 14:47:46 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+  trailingSlash: true, // Ensures pages are saved as folders with index.html
 };
 
 export default nextConfig;
